6c4d075017c7974114e04c10bcc2a9de96be7c3f
Martin Holmin <martin.holmin@me.com>
2016-10-19 20:55:51 +0100

    WIP: Site layout

---
+++ _strengths/streaming-analytics-on-android.markdown
@@ -0,0 +1,6 @@
+---
+title: Streaming Analytics on Android
+date: 2016-10-19 21:48:00 +02:00
+---
+
+sa.engine is the only complex real-time streaming anytics system running on Android. Our minimal footprint together with our resouce efficient high level query language provides a range of new and disruptive use cases.
--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,7 +1,25 @@
-//
-// Jekyll-Materialize-Boilerplate
-// Author: @newaeonweb
-//
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
 
 // Init Materialize javascripts
 (function($){
@@ -9,5 +27,5 @@
 
     $('.button-collapse').sideNav();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+})(jQuery);
